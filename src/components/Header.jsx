42ea31e944374382967ad50594b6a69b877fcf9f
diana <d.herreroc97@gmail.com>
2025-05-14 12:14:51 +0900

    general header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,45 @@
 import { Link, NavLink } from "react-router-dom"
+import photo from "../assets/Photo.png"
 
 export default function Header(){
     return(
         <header>
-            <Link  to="/">Diana Herrero</Link>
-            <NavLink
-                to="/"
-            >
-                About me
-            </NavLink>
-            <NavLink
-                to="/skills"
-            >
-                Skills
-            </NavLink>
-            <NavLink
-                to="/education"
-            >
-                Education
-            </NavLink>
-            <NavLink
-                to="/workhistory"
-            >
-                Work history
-            </NavLink>
-            <NavLink
-                to="/contact"
-            >
-                Contact
-            </NavLink>
+            <Link  to="/" className="my-logo">
+                <img src={photo} alt="my picture"/>
+                <span>Diana Herrero</span>
+            </Link>
+            <nav>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => isActive ? "active-nav-link" : null}
+                >
+                    About me
+                </NavLink>
+                <NavLink
+                    to="/skills"
+                    className={({ isActive }) => isActive ? "active-nav-link" : null}
+                >
+                    Skills
+                </NavLink>
+                <NavLink
+                    to="/education"
+                    className={({ isActive }) => isActive ? "active-nav-link" : null}
+                >
+                    Education
+                </NavLink>
+                <NavLink
+                    to="/workhistory"
+                    className={({ isActive }) => isActive ? "active-nav-link" : null}
+                >
+                    Work history
+                </NavLink>
+                <NavLink
+                    to="/contact"
+                    className={({ isActive }) => isActive ? "active-nav-link" : null}
+                >
+                    Contact
+                </NavLink>
+            </nav>
         </header>
     )
 }
