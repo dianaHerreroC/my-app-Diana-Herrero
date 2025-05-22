62d6bf5a5ce07d4658b1f768214aa26d3a4f2dfe
diana <d.herreroc97@gmail.com>
2025-05-22 13:12:00 +0900

    done contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,25 @@
+import { FiPhone } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
+import { SlLocationPin } from "react-icons/sl";
+
 export default function Contact(){
     return(
-        <div>
-            <h1>Contact page</h1>
+        <div className="contact-section">
+            <h1 className="titles">Contact details</h1>
+            <div className="contact-details">
+                <span>
+                    <FiPhone className="icon" />
+                    <p className="highlighted-text">(+34) 679 202 800</p>
+                </span>
+                <span>
+                    <FiMail className="icon" />
+                    <p className="highlighted-text">d.herreroc97@gmail.com</p>
+                </span>
+                <span>
+                    <SlLocationPin className="icon" />
+                    <p className="highlighted-text">Madrid</p>
+                </span>
+            </div>
         </div>
     )
 }
