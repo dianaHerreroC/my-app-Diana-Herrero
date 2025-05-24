8b2ab648c25c6b057297b8fac92a8ec8dfed529a
diana <d.herreroc97@gmail.com>
2025-05-24 13:30:25 +0900

    copy and link in mail and number

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,43 @@
-import { FiPhone } from "react-icons/fi";
-import { FiMail } from "react-icons/fi";
-import { SlLocationPin } from "react-icons/sl";
+import { FiPhone } from "react-icons/fi"
+import { FiMail } from "react-icons/fi"
+import { SlLocationPin } from "react-icons/sl"
+import { MdContentCopy } from "react-icons/md"
+import { useState } from "react"
 
 export default function Contact(){
+    const [emailCopied, setEmailCopied] = useState(false)
+    const [phoneCopied, setPhoneCopied] = useState(false)
+    const handleEmailCopy = () => {
+        navigator.clipboard.writeText("d.herreroc97@gmail.com")
+        setEmailCopied(true)
+        setTimeout(() => setEmailCopied(false), 1000) // Hide tooltip after 2s
+    }
+    const handlePhoneCopy = () => {
+        navigator.clipboard.writeText("+34679202800")
+        setPhoneCopied(true)
+        setTimeout(() => setPhoneCopied(false), 1000) // Hide tooltip after 2s
+    }
     return(
         <div className="contact-section">
             <h1 className="titles">Contact details</h1>
             <div className="contact-details">
-                <span>
+                <span className="contact-detail">
                     <FiPhone className="icon" />
-                    <p className="highlighted-text">(+34) 679 202 800</p>
+                    <a className="highlighted-text" href="tel:+34679202800" target="_blank">(+34) 679 202 800</a>
+                    <span className="copy">
+                        <MdContentCopy className="copy-icon" onClick={handlePhoneCopy}/>
+                        {phoneCopied && <span className="copied-tooltip small-messages">Copied!</span>}
+                    </span>
                 </span>
-                <span>
+                <span className="contact-detail">
                     <FiMail className="icon" />
-                    <p className="highlighted-text">d.herreroc97@gmail.com</p>
+                    <a className="highlighted-text" href="mailto:d.herreroc97@gmail.com" target="_blank"> d.herreroc97@gmail.com </a>
+                    <span className="copy">
+                        <MdContentCopy className="copy-icon" onClick={handleEmailCopy}/>
+                        {emailCopied && <span className="copied-tooltip small-messages">Copied!</span>}
+                    </span>
                 </span>
-                <span>
+                <span className="contact-detail">
                     <SlLocationPin className="icon" />
                     <p className="highlighted-text">Madrid</p>
                 </span>
